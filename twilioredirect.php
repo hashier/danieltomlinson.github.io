39721c46cbe33798355621d25a62cdadd939375c
Daniel Tomlinson <Daniel@ShadowDevelopments.co.uk>
2013-11-14 14:00:00 +0100

    Remove old php stuff

--- twilioredirect.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-echo "<Response><Dial>" . ($_REQUEST['Digits'] == 1 ? "07851335666" : ($_REQUEST['Digits'] == 2 ? "07519817363" : "07934726875")) . "</Dial></Response>";
-?>
